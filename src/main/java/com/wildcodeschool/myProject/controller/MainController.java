6e881ba3cc75f91d1f5772b77ce25a7c9e0278bf
Alexandre L <108535805+Newaluigi@users.noreply.github.com>
2023-07-03 23:22:06 +0200

    first commit

---
+++ src/main/java/com/wildcodeschool/myProject/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.wildcodeschool.myProject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/greetings")
+    @ResponseBody
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
